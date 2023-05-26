3454dea21438d817850abc8fca00548cfbd6f9a7
Josh Cline <joshccline@outlook.com>
2023-05-25 22:39:07 -0400

    Made counter using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const Increment = () => {
+    setCounter(currentVal => currentVal + 1)
+  }
+
+  const Decrement = () => {
+    if (counter === 0) return
+    setCounter(currentVal => currentVal -1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Count: {counter}</h1>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
     </div>
   );
 }
